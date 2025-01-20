b2f9e13616594c37cf876b456225e337d79ec8aa
chernogor <fyodorchernogor@gmail.com>
2025-01-20 19:48:25 +0300

    train, readme

--- train.sh
+++ train.sh
@@ -1,15 +1,6 @@
 python -m src.train \
-    --exp nonorm \
-    --lr 0.001 \
-    --ep 200
-
-# python -m src.train \
-#     --exp notrans \
-#     --notrans \
-#     --ep 300
-
-
-# python -m src.train \
-#     --exp res_long \
-#     --save 400 \
-#     --ep 3000
+    --exp bs5bn \
+    --bs 5 \
+    --lr 0.0003 \
+    --save 200 \
+    --ep 500
